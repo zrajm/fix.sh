328a0e3c8a51c15dcc9f3472dafbf7f73910ff38
zrajm <zrajm@klingonska.org>
2014-06-27 00:18:06 +0200

    Refuse to overwrite target files modified by user.
    * Overrideable with the $FIX_FORCE environment variable.

--- fix.sh
+++ fix.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env dash
 
+# May be set by user:
+#   * FIX_FORCE
+
 ##############################################################################
 ##                                                                          ##
 ##  Functions                                                               ##
@@ -12,31 +15,64 @@ error() {
     exit 1
 }
 
+mkpath() {
+    local DIR="${1%/*}"                        # strip trailing filename
+    [ -d "$DIR" ] || mkdir -p -- "$DIR"
+}
+
+meta_checksum() {
+    local FILE="$1" CHECKSUM=""
+    [ -e "$FILE" ] && read CHECKSUM <"$FILE"
+    echo "${CHECKSUM%% *}"                     # checksum without filename
+}
+
+file_checksum() {
+    local FILE="$1" CHECKSUM=""
+    [ -e "$FILE" ] && CHECKSUM="$(sha1sum "$FILE")"
+    echo "${CHECKSUM%% *}"                     # checksum without filename
+}
+
 build() {
+    local DBFILE="$FIX_DIR/$1"
     local SCRIPT="$FIX_SCRIPT_DIR/$1.fix"
     local TARGET="$FIX_TARGET_DIR/$1"
-    local TARGET_DIR="${TARGET%/*}"
     local TARGET_TMP="$TARGET--fixing"
+    local STATUS=0 STORED_CHECKSUM=""
+    local OLD_TARGET_CHECKSUM="" NEW_TARGET_CHECKSUM=""
 
-    # check preconditions
-    [ -e "$SCRIPT" ] || error "build script '$SCRIPT' does not exist"
-    [ -x "$SCRIPT" ] || error "build script '$SCRIPT' is not executable"
+    # check build script
+    [ -e "$SCRIPT" ] || error "Build script '$SCRIPT' does not exist"
+    [ -x "$SCRIPT" ] || error "Build script '$SCRIPT' is not executable"
 
     # run buildscript
-    [ -e "$TARGET_DIR" ] || mkdir -p -- "$TARGET_DIR"
+    mkpath "$TARGET" || error "Can't create dir for target '$TARGET'"
     "$SCRIPT" >"$TARGET_TMP" <&-
-    local STATUS=$?                            # check buildscript exit status
+    STATUS=$?                                  # check buildscript exit status
     if [ $STATUS -ne 0 ]; then
-        error "build script '$SCRIPT' returned exit status $STATUS" \
-            "Target unmodified; script output can be found in '$TARGET_TMP'."
+        error "Build script '$SCRIPT' returned exit status $STATUS" \
+            "Old target unchanged. New, failed target written to '$TARGET_TMP'."
     fi
-    # new target is same as old target, keep old as-is
-    if diff "$TARGET" "$TARGET_TMP" >/dev/null; then
-        rm -f -- "$TARGET_TMP"
-        return
+
+    STORED_CHECKSUM="$(meta_checksum "$DBFILE")"
+    OLD_TARGET_CHECKSUM="$(file_checksum "$TARGET")"
+    NEW_TARGET_CHECKSUM="$(file_checksum "$TARGET_TMP")"
+
+    if [ "$OLD_TARGET_CHECKSUM" = "$NEW_TARGET_CHECKSUM" ]; then
+        rm -f -- "$TARGET_TMP"                 # new is same as old
+    elif [ "$FIX_FORCE" -o ! -e "$TARGET" \
+        -o "$OLD_TARGET_CHECKSUM" = "$STORED_CHECKSUM" ]; then
+        mv -f -- "$TARGET_TMP" "$TARGET"       # old written by fix or no old
+    else
+        error "Old target '$TARGET' modified by user, won't overwrite" \
+            "Erase old target before rebuild. New target kept in '$TARGET_TMP'."
     fi
-    # new target is different than old target, keep new
-    mv -f -- "$TARGET_TMP" "$TARGET"
+
+    # metadata unchanged
+    [ "$NEW_TARGET_CHECKSUM" = "$STORED_CHECKSUM" ] && return
+
+    mkpath "$DBFILE" || error "can't create dir for buildstat file '$DBFILE'"
+    echo "$NEW_TARGET_CHECKSUM $TARGET" >"$DBFILE"
+    return
 }
 
 ##############################################################################
@@ -49,7 +85,7 @@ if [ ! "$FIX" ]; then                          # mother process
     export FIX="$(readlink -f $0)"
     export FIX_LEVEL=0
     export FIX_PID=$$
-    export FIX_DIR="$PWD/.fix"
+    export FIX_DIR=".fix"
     export FIX_SCRIPT_DIR="fix"
     export FIX_SOURCE_DIR="src"
     export FIX_TARGET_DIR="build"
