ebc04e6b45a95ab5423e55ba60eee9da798e14f4
zrajm <zrajm@klingonska.org>
2014-06-26 21:20:40 +0200

    Very simplistic 'fix.sh'.

---
+++ fix.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env dash
+
+##############################################################################
+##                                                                          ##
+##  Functions                                                               ##
+##                                                                          ##
+##############################################################################
+
+error() {
+    echo "ERROR: $1" >&2
+    [ "$2" ] && echo "    ($2)" >&2
+    exit 1
+}
+
+build() {
+    local SCRIPT="$FIX_SCRIPT_DIR/$1.fix"
+    local TARGET="$FIX_TARGET_DIR/$1"
+    local TARGET_DIR="${TARGET%/*}"
+    local TARGET_TMP="$TARGET--fixing"
+
+    # check preconditions
+    [ -e "$SCRIPT" ] || error "build script '$SCRIPT' does not exist"
+    [ -x "$SCRIPT" ] || error "build script '$SCRIPT' is not executable"
+
+    # run buildscript
+    [ -e "$TARGET_DIR" ] || mkdir -p -- "$TARGET_DIR"
+    "$SCRIPT" >"$TARGET_TMP" <&-
+
+    # check buildscript status
+    local STATUS=$?
+    if [ $STATUS -ne 0 ]; then
+        error "build script '$SCRIPT' returned exit status $STATUS" \
+            "Target unmodified; script output can be found in '$TARGET_TMP'."
+    else
+        mv -f -- "$TARGET_TMP" "$TARGET"
+    fi
+}
+
+##############################################################################
+##                                                                          ##
+##  Init                                                                    ##
+##                                                                          ##
+##############################################################################
+
+if [ ! "$FIX" ]; then                          # mother process
+    export FIX="$(readlink -f $0)"
+    export FIX_LEVEL=0
+    export FIX_PID=$$
+    export FIX_DIR="$PWD/.fix"
+    export FIX_SCRIPT_DIR="fix"
+    export FIX_SOURCE_DIR="src"
+    export FIX_TARGET_DIR="build"
+    [ -d "$FIX_SOURCE_DIR" ] || error "source dir '$FIX_SOURCE_DIR' does not exist"
+    [ -d "$FIX_SCRIPT_DIR" ] || error "script dir '$FIX_SCRIPT_DIR' does not exist"
+else                                           # child
+    FIX_LEVEL=$(( FIX_LEVEL + 1 ))
+fi
+
+##############################################################################
+##                                                                          ##
+##  Main                                                                    ##
+##                                                                          ##
+##############################################################################
+
+while [ $# -gt 0 ]; do                         # for each argument
+    build $1
+    shift
+done
+
+#[eof]
