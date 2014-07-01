e6c680797cd0b1d5de86a796d13deb9bf76d97e3
zrajm <zrajm@klingonska.org>
2014-07-01 08:39:34 +0200

    Refactored. Wrote pseudocode, started with tests.

--- fix.sh
+++ fix.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env dash
 
 # May be set by user:
+#   * FIX_DEBUG
 #   * FIX_FORCE
 
 ##############################################################################
@@ -8,6 +9,10 @@
 ##  Functions                                                               ##
 ##                                                                          ##
 ##############################################################################
+
+debug() {
+    [ "$FIX_DEBUG" ] && echo "$1" >&2
+}
 
 error() {
     echo "ERROR: $1" >&2
@@ -32,48 +37,67 @@ file_checksum() {
     echo "${CHECKSUM%% *}"                     # checksum without filename
 }
 
-build() {
-    local DBFILE="$FIX_DIR/$1"
-    local SCRIPT="$FIX_SCRIPT_DIR/$1.fix"
-    local TARGET="$FIX_TARGET_DIR/$1"
-    local TARGET_TMP="$TARGET--fixing"
-    local STATUS=0 STORED_CHECKSUM=""
-    local OLD_TARGET_CHECKSUM="" NEW_TARGET_CHECKSUM=""
+run() {
+    local CMD="$1" STATUS=0
 
     # check build script
-    [ -e "$SCRIPT" ] || error "Build script '$SCRIPT' does not exist"
-    [ -x "$SCRIPT" ] || error "Build script '$SCRIPT' is not executable"
+    [ -e "$CMD" ] || error "Build script '$CMD' does not exist"
+    [ -r "$CMD" ] || error "No read permission for build script '$CMD'"
+    [ -x "$CMD" ] || error "No execute permission for build script '$CMD'"
 
     # run buildscript
-    mkpath "$TARGET" || error "Can't create dir for target '$TARGET'"
-    "$SCRIPT" >"$TARGET_TMP" <&-
+    "$CMD"
     STATUS=$?                                  # check buildscript exit status
     if [ $STATUS -ne 0 ]; then
-        error "Build script '$SCRIPT' returned exit status $STATUS" \
+        error "Build script '$CMD' returned exit status $STATUS" \
             "Old target unchanged. New, failed target written to '$TARGET_TMP'."
     fi
+}
 
-    STORED_CHECKSUM="$(meta_checksum "$DBFILE")"
-    OLD_TARGET_CHECKSUM="$(file_checksum "$TARGET")"
-    NEW_TARGET_CHECKSUM="$(file_checksum "$TARGET_TMP")"
+store_result() {
+    local DBFILE="$1" TARGET="$2" TARGET_TMP="$3"
 
-    if [ "$OLD_TARGET_CHECKSUM" = "$NEW_TARGET_CHECKSUM" ]; then
-        rm -f -- "$TARGET_TMP"                 # new is same as old
-    elif [ "$FIX_FORCE" -o ! -e "$TARGET" \
-        -o "$OLD_TARGET_CHECKSUM" = "$STORED_CHECKSUM" ]; then
-        mv -f -- "$TARGET_TMP" "$TARGET"       # old written by fix or no old
+    local META_CHECKSUM="$(meta_checksum "$DBFILE")"
+    local OLD_CHECKSUM="$(file_checksum "$TARGET")"
+    local NEW_CHECKSUM="$(file_checksum "$TARGET_TMP")"
+
+    # update target
+    if [ ! -e "$TARGET" ]; then
+        debug "$TARGET: No previous target, write new target"
+        mv -f -- "$TARGET_TMP" "$TARGET"
+    elif [ "$NEW_CHECKSUM" = "$OLD_CHECKSUM" ]; then
+        debug "$TARGET: Target unchanged, keep old target"
+        rm -f -- "$TARGET_TMP"
+    elif [ -e "$DBFILE" -a "$OLD_CHECKSUM" = "$META_CHECKSUM" ]; then
+        debug "$TARGET: Target updated, write new target"
+        mv -f -- "$TARGET_TMP" "$TARGET"
+    elif [ "$FIX_FORCE" ]; then
+        debug "$TARGET: Target updated + external change, forced overwrite"
+        mv -f -- "$TARGET_TMP" "$TARGET"
     else
         error "Old target '$TARGET' modified by user, won't overwrite" \
             "Erase old target before rebuild. New target kept in '$TARGET_TMP'."
     fi
 
-    # metadata unchanged
-    [ "$NEW_TARGET_CHECKSUM" = "$STORED_CHECKSUM" ] && return
-
-    mkpath "$DBFILE" || error "can't create dir for buildstat file '$DBFILE'"
-    echo "$NEW_TARGET_CHECKSUM $TARGET" >"$DBFILE"
-    return
+    # update metadata
+    if [ "$NEW_CHECKSUM" != "$META_CHECKSUM" ]; then
+        mkpath "$DBFILE" || error "Cannot create dir for metadata file '$DBFILE'"
+        debug "$TARGET: Writing metadata"
+        echo "$NEW_CHECKSUM $TARGET" >"$DBFILE"
+    else
+        debug "$TARGET: Metadata is up-to-date"
+    fi
 }
+
+build() {
+    local DBFILE="$FIX_DIR/$1"
+    local SCRIPT="$FIX_SCRIPT_DIR/$1.fix"
+    local TARGET="$FIX_TARGET_DIR/$1"
+    mkpath "$TARGET" || error "Cannot create dir for target '$TARGET'"
+    run "$SCRIPT" >"$TARGET--fixing" <&-
+    store_result "$DBFILE" "$TARGET" "$TARGET--fixing"
+}
+
 
 ##############################################################################
 ##                                                                          ##
@@ -82,6 +106,7 @@ build() {
 ##############################################################################
 
 if [ ! "$FIX" ]; then                          # mother process
+    # FIX_FORCE FIX_DEBUG
     export FIX="$(readlink -f $0)"
     export FIX_LEVEL=0
     export FIX_PID=$$
