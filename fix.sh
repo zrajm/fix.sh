3be793574dfdf4b18eb54f2202d227473e80d53f
zrajm <zrajm@klingonska.org>
2015-11-12 23:24:06 +0100

    Renamed function error() -> die().

--- fix.sh
+++ fix.sh
@@ -19,7 +19,7 @@ debug() {
     return 0
 }
 
-error() {
+die() {
     local STATUS="$1" MSG="$2" EXTRA="$3"
     echo "ERROR: $MSG" >&2
     [ "$EXTRA" ] && echo "    ($EXTRA)" >&2
@@ -51,7 +51,7 @@ is_mother() {
 
 establish_lock() {
     local LOCKFILE="$1" SIG
-    mkpath "$LOCKFILE" || error 7 "Cannot create dir for lockfile '$LOCKFILE'"
+    mkpath "$LOCKFILE" || die 7 "Cannot create dir for lockfile '$LOCKFILE'"
     ({ set -o noclobber; echo "$$" >"$LOCKFILE"; } 2>/dev/null) || return 1
     trap "rm -f '$LOCKFILE'" EXIT
     for SIG in HUP INT TERM; do
@@ -63,13 +63,13 @@ establish_lock() {
 # Run buildscript, write tempfile. React to exit status.
 build_run() {
     local CMD="$1" TMPFILE="$2"
-    [ -e "$CMD" ] || error 1 "Buildscript '$CMD' does not exist"
-    [ -r "$CMD" ] || error 1 "No read permission for buildscript '$CMD'"
-    [ -x "$CMD" ] || error 1 "No execute permission for buildscript '$CMD'"
+    [ -e "$CMD" ] || die 1 "Buildscript '$CMD' does not exist"
+    [ -r "$CMD" ] || die 1 "No read permission for buildscript '$CMD'"
+    [ -x "$CMD" ] || die 1 "No execute permission for buildscript '$CMD'"
 
     # FIXME: Catch a failure to write to $TMPFILE
     "$CMD" >"$TMPFILE" <&- &                   # run buildscript
-    wait "$!" || error 1 "Buildscript '$CMD' returned exit status $?" \
+    wait "$!" || die 1 "Buildscript '$CMD' returned exit status $?" \
         "Old target unchanged. New, failed target written to '$TMPFILE'."
 }
 
@@ -95,21 +95,21 @@ build_finalize() {
         debug "$TARGET: Target updated + external change, forced overwrite"
         mv -f -- "$TARGET_TMP" "$TARGET"
     else
-        error 1 "Old target '$TARGET' modified by user, won't overwrite" \
+        die 1 "Old target '$TARGET' modified by user, won't overwrite" \
             "Erase old target before rebuild. New target kept in '$TARGET_TMP'."
     fi
 
     # update metadata
     if [ "$NEW_CHECKSUM" != "$META_CHECKSUM" ]; then
         mkpath "$DBFILE" \
-            || error 7 "Cannot create dir for metadata file '$DBFILE'"
+            || die 7 "Cannot create dir for metadata file '$DBFILE'"
         debug "$TARGET: Writing metadata"
 
         case "$TYPE" in
             SCRIPT) FILE="${SCRIPT#$FIX_SCRIPT_DIR/}" ;;
             SOURCE) FILE="${SOURCE#$FIX_SOURCE_DIR/}" ;;
             TARGET) FILE="${TARGET#$FIX_TARGET_DIR/}" ;;
-            *) error 30 "build_finalize: Failed to write metadata" \
+            *) die 30 "build_finalize: Failed to write metadata" \
                 "Internal error: unknown type '$TYPE' for file '$TARGET'" ;;
         esac
         echo "$NEW_CHECKSUM $TYPE $FILE" >"$DBFILE"
@@ -122,7 +122,7 @@ build() {
     local DBFILE="$FIX_DIR/state/$1" \
         SCRIPT="$FIX_SCRIPT_DIR/$1.fix" \
         TARGET="$FIX_TARGET_DIR/$1"
-    mkpath "$TARGET" || error 6 "Cannot create dir for target '$TARGET'"
+    mkpath "$TARGET" || die 6 "Cannot create dir for target '$TARGET'"
     build_run "$SCRIPT" "$TARGET--fixing"
     build_finalize "$DBFILE" TARGET "$TARGET" "$TARGET--fixing"
 }
@@ -133,7 +133,7 @@ build() {
 ##                                                                          ##
 ##############################################################################
 
-[ "$#" = 0 ] && error 15 "No target(s) specified"
+[ "$#" = 0 ] && die 15 "No target(s) specified"
 if is_mother; then                             # mother process
     # FIX_FORCE FIX_DEBUG
     export FIX="$(readlink -f "$0")"
@@ -145,13 +145,13 @@ if is_mother; then                             # mother process
     export FIX_SOURCE_DIR="src"
     export FIX_TARGET_DIR="build"
     [ -n "$FIX_SOURCE" ] \
-        && error 15 "Option '--source' can only be used inside buildscript"
+        && die 15 "Option '--source' can only be used inside buildscript"
     [ -d "$FIX_SOURCE_DIR" ] \
-        || error 10 "Source dir '$FIX_SOURCE_DIR' does not exist"
+        || die 10 "Source dir '$FIX_SOURCE_DIR' does not exist"
     [ -d "$FIX_SCRIPT_DIR" ] \
-        || error 10 "Script dir '$FIX_SCRIPT_DIR' does not exist"
+        || die 10 "Script dir '$FIX_SCRIPT_DIR' does not exist"
     establish_lock "$FIX_LOCK" \
-        || error 8 "Cannot create lockfile '$FIX_LOCK'" \
+        || die 8 "Cannot create lockfile '$FIX_LOCK'" \
         "Is ${FIX##*/} is already running? Is lockfile dir writeable?"
 else                                           # child
     FIX_LEVEL="$(( FIX_LEVEL + 1 ))"
